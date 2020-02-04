8e4e55c554ff5a1ffae53b3ac93f367905db3b2e
Federico Benassi <benassi.federico.sagittarius@gmail.com>
2020-02-04 13:03:43 +0100

    created algorithm-implementation project
    Implementation of:
    1. Linear Search
    2. Binary Search
    3. Jump Search
    For only integers array

--- algorithm-implementation/src/main/java/federico/benassi/Main.java
+++ algorithm-implementation/src/main/java/federico/benassi/Main.java
@@ -1,3 +1,5 @@
+package federico.benassi;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Algorithm Implementation");
---
+++ algorithm-implementation/src/main/java/federico/benassi/linear_search/JumpSearch.java
@@ -0,0 +1,30 @@
+package federico.benassi.linear_search;
+
+public class JumpSearch {
+
+    // Work only on sorted arrays and worst case O(n/m)
+    public static int jumpSearch(int[] arr, int elementToBeFound){
+        int m = (int) Math.sqrt(arr.length);
+
+        for(int i = 0; i < arr.length; i += m){
+            if(arr[i] > elementToBeFound){
+                for(int j = i - m; j < i; j++){
+                    if(arr[j] == elementToBeFound) return j;
+                }
+                return -1;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[1000];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = i;
+        }
+
+        System.out.println(jumpSearch(arr, 53));
+    }
+}
